8c69fe2f237907422c4bb2a30853525c41965918
Veronika Teplova <teplovavo@gmail.com>
2024-11-04 15:03:24 -0700

    basic code for connectivity

---
+++ script.js
@@ -0,0 +1,33 @@
+// Importing packages and Grade model
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import Grade from './models/Base.js';
+
+// Initialize app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Function to connect to MongoDB
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.ATLAS_URI);
+        console.log('Connected to Mongo!');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        process.exit(1); // Exit process with failure
+    }
+}
+
+// Call function to connect to database
+connectDB();
+
+// Middleware for JSON parsing
+app.use(express.json());
+
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
